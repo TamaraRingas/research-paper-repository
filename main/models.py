7e7cdea87e03bbb0ec52cafb91b2bc452c5c14bb
ryanwolhuter <ryan@continuon.co>
2021-08-20 13:40:34 +0200

    add authors to paper model

--- main/models.py
+++ main/models.py
@@ -5,6 +5,8 @@ class Paper(models.Model):
   """This class represents a research paper uploaded to our database, derived from the Model class."""
 
   name = models.CharField(max_length=100)
-  abstract = models.CharField(max_length=250)
+  abstract = models.TextField(max_length=500)
+  authors = models.ManyToManyField()
   year = models.IntegerField()
+  pdf = models.FileField()
   peerReview = models.FileField()
