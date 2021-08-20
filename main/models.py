741e2b03af0ccec093d190f848b7586bba52e14e
ryanwolhuter <ryan@continuon.co>
2021-08-20 12:34:49 +0200

    first commit

---
+++ main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Paper(models.Model):
+  """This class represents a research paper uploaded to our database, derived from the Model class."""
+
+  name = models.CharField(max_length=100)
+  abstract = models.CharField(max_length=250)
+  year = models.IntegerField()
+  peerReview = models.FileField()
