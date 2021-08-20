981aba88768865175572729de8331bca6e843aaf
Tamara Ringas <tamriasdev@gmail.com>
2021-08-20 20:41:37 +0200

    Add author model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Paper(models.Model):
@@ -7,22 +8,37 @@ class Paper(models.Model):
     name = models.CharField(max_length=100)
     abstract = models.TextField(max_length=1000)
     year = models.IntegerField()
-    authors = models.ManyToManyField()
-    researchGroup = models.ManyToManyField()
-    venue = models.CharField()
+    authors = models.ManyToManyField("Author", blank=True)
+    #researchGroup = models.ManyToManyField()
+    venue = models.CharField(max_length=50)
     pdf = models.FileField(default=' ', upload_to='media/',
                            verbose_name="Research Paper")
     peerReview = models.FileField(
-        default=' ', upload_to='media/', verbose_name="Peer Review")
+        default=' ', upload_to='media/', verbose_name="Proof of Peer Review")
 
-    class Meta: 
-        ordering = ['-year', 'authors']
+    # Order reserach papers by year (descending) then by authors.
+    class Meta:
+        ordering = ['-year']
 
     def get_absolute_url(self):
-      """Returns the url to access a particular research paper."""
-      return reverse('page-detail', args=[str(self.id)])
+        """Returns the url to access a particular research paper."""
+        return reverse('paper-detail', args=[str(self.id)])
 
     def __str__(self):
-        return self.name 
+        return self.name
 
 
+class Author(models.Model):
+    """This class represents an author, derived from the Model class"""
+
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    papers = models.ManyToManyField(Paper)
+    #researchGroup = models.ManyToManyField()
+
+    # Order authors by their surnames.
+    class Meta:
+        ordering = ['surname']
+
+    def __str__(self):
+        return f'{self.surname}, {self.name}'
