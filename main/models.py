1e2323fc10a12906257c38d12027409682c4a05a
Tamara Ringas <tamriasdev@gmail.com>
2021-08-20 21:04:15 +0200

    Alter field papers on author

--- main/models.py
+++ main/models.py
@@ -34,7 +34,7 @@ class Author(models.Model):
 
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
-    papers = models.ManyToManyField(Paper)
+    papers = models.ManyToManyField(Paper, blank=True)
     #researchGroup = models.ManyToManyField()
 
     # Order authors by their surnames.
