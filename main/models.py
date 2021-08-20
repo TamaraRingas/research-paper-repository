fa9d3ac956087af1646c630e5e202d173f5dbca5
ryanwolhuter <ryan@continuon.co>
2021-08-20 16:53:52 +0200

    add ordering metadata to models.py

--- main/models.py
+++ main/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class Paper(models.Model):
-  """This class represents a research paper uploaded to our database, derived from the Model class."""
+    """This class represents a research paper uploaded to our database, derived from the Model class."""
 
-  name = models.CharField(max_length=100)
-  abstract = models.TextField(max_length=500)
-  authors = models.ManyToManyField()
-  year = models.IntegerField()
-  pdf = models.FileField()
-  peerReview = models.FileField()
+    name = models.CharField(max_length=100)
+    abstract = models.TextField(max_length=1000)
+    year = models.IntegerField()
+    authors = models.ManyToManyField()
+    researchGroup = models.ManyToManyField()
+    venue = models.CharField()
+    pdf = models.FileField(default=' ', upload_to='media/',
+                           verbose_name="Research Paper")
+    peerReview = models.FileField(
+        default=' ', upload_to='media/', verbose_name="Peer Review")
+
+    class Meta:
+        ordering = ['-year', 'authors']
+
+    def __str__(self):
+        return self.name
