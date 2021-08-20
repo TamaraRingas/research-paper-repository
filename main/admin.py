71c722689a8d682a42b914ab07d9aeba663588b6
Tamara Ringas <tamriasdev@gmail.com>
2021-08-20 21:31:17 +0200

    Register models with their admins

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from .models import Paper, Author, ResearchGroup
 
 # Register your models here.
-admin.site.register(Paper)
-admin.site.register(Author)
-admin.site.register(ResearchGroup)
+@admin.register(Paper)
+class PaperAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(Paper, PaperAdmin)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+  pass
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+@admin.register(ResearchGroup)
+class ResearchGroupAdmin(admin.ModelAdmin):
+  pass
+
+
+admin.site.register(ResearchGroup, ResearchGroupAdmin)
