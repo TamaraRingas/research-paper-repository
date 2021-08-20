b8ef8990155c2eaf337dd326a15c75966813ba5d
Tamara Ringas <tamriasdev@gmail.com>
2021-08-20 18:18:02 +0200

    add PageList View

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from models import Page
 
-def PageView():
+
+# View class that gets and displays video list.
+class PageListView(generic.ListView):
+    # Inherited from the ListView Class
+    model = Page
+    paginate_by = 10
+
+    def get_queryset(self):  # Filter results by input query.
+      query = self.request.GET.get('query', None)
+
+      if query:
+        return Page.objects.filter(name__icontains=query)
+
+      else:
+        return Page.objects.all()
