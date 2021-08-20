26ab0154829cd6fe48e2835dc05b9e5dbe8f8b78
Tamara Ringas <tamriasdev@gmail.com>
2021-08-20 21:33:12 +0200

    use admin decorator

--- main/admin.py
+++ main/admin.py
@@ -1,25 +1,15 @@
 from django.contrib import admin
 from .models import Paper, Author, ResearchGroup
 
-# Register your models here.
+"""Models registered here"""
 @admin.register(Paper)
 class PaperAdmin(admin.ModelAdmin):
   pass
 
-admin.site.register(Paper, PaperAdmin)
-
-
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
   pass
-
-
-admin.site.register(Author, AuthorAdmin)
-
-
 @admin.register(ResearchGroup)
 class ResearchGroupAdmin(admin.ModelAdmin):
   pass
 
-
-admin.site.register(ResearchGroup, ResearchGroupAdmin)
